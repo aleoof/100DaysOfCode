f5503da35e8a362e0df47f7e065ba8ea5784dc71
Alexandre <alexandre_osvaldo@hotmail.com>
2019-09-10 17:46:26 -0300

    arquivo de db cria tabelas

--- server-api/bd-conect.js
+++ server-api/bd-conect.js
@@ -1,21 +1,65 @@
 const mysql = require('mysql')
 
-const conection = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: '',
-    database: 'blog_database'
-});
+exports.creationDatabase = (req, res) => {
+    const connection = mysql.createConnection({
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: '',
+        database: 'blog_database'
+    });
+    
+    connection.connect((err) => {
+        if(err){
+            return console.log(err)
+        }
+        console.log('conectou');
+        database(connection);
+        createUsersTable(connection);
+        createPostsTable(connection);
+    })
+}
 
-conection.connect((err) => {
-    if(err){
-        return console.log(err)
-    }
-    console.log('conectou')
-})
 
-function createTables(coon){
-    const sql = "CREATE TABLE IF NOT EXISTS Clientes (\n"+
-    "id int NOT NULL /n"+
+function database(conn){
+    const sql = "CREATE IF NOT EXISTS DATABASE blog";
+    conn.query(sql, (err, result, field) => {
+        if(err){
+            return console.log(err);
+        }
+        console.log("Created database")
+    })
+}
+
+function createUsersTable(coon){
+    const sql = "CREATE TABLE IF NOT EXISTS Users (\n"+
+    "id INT NOT NULL /n"+
+    "name VARCHAR(100) NOT NULL /n"+
+    "email VARCHAR(100) NOT NULL /n"+
+    "password VARCHAR(100) NOT NULL /n"+
+    ")";
+
+    conn.query(sql, (err, result, field) => {
+        if(err){
+            return console.log(err);
+        }
+        console.log("Created table Users")
+    })
+}
+
+function createPostsTable(coon){
+    const sql = "CREATE TABLE IF NOT EXISTS Posts (\n"+
+    "id INT NOT NULL /n"+
+    "title VARCHAR(100) NOT NULL /n"+
+    "image TEXT NOT NULL /n"+
+    "post TEXT NOT NULL /n"+
+    ")"+
+    "ALTER TABLE Posts ADD CONSTRAINT 'fk_user' FOREIGN KEY ('id') REFERENCES 'Users' ('id') ";
+
+    conn.query(sql, (err, result, field) => {
+        if(err){
+            return console.log(err);
+        }
+        console.log("Created table Posts")
+    })
 }
--- server-api/index.js
+++ server-api/index.js
@@ -3,11 +3,13 @@ const bodyParser = require('body-parser')
 const app = express();
 const routers = express.Router();
 const port = 3000;
+const mysql = require('./bd-conect');
 
 const postRoute = require ('./routes/posts-routers')
 const userRoute = require('./routes/users-routers')
 
 app.listen(port, () => {
+    mysql.creationDatabase();
     console.log(`App is running on port http://localhost:${port}`)
 })
 
