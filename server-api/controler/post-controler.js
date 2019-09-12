9fa1c6f24e2c766a267985a44a3389aedbd60259
Alexandre <alexandre_osvaldo@hotmail.com>
2019-09-12 17:30:27 -0300

    Ajustes de codigo

--- server-api/controler/post-controler.js
+++ server-api/controler/post-controler.js
@@ -1,14 +1,14 @@
-const PostModel = require('../models/post')
+const PostModel = require('../models/post');
 
-const sql = require('../bd-conect')
+const sql = require('../bd-conect');
 
 exports.get = async (req, res, next) => {
-    let getAllPost = sql.creationDatabase('SELECT * FROM Post')
-    try {
-        res.status(200).send(getAllPost)
-    } catch (e) {
-        res.status(500).send({
-            message: 'Error on process request'
-        })
-    }
-}
+  let getAllPost = sql.creationDatabase('SELECT * FROM Post');
+  try {
+    res.status(200).send(getAllPost);
+  } catch (e) {
+    res.status(500).send({
+      message: 'Error on process request',
+    });
+  }
+};
--- server-api/controler/user-controler.js
+++ server-api/controler/user-controler.js
@@ -1,22 +1,22 @@
-const userModel = require('../models/user')
+const userModel = require('../models/user');
 
-const sql = require('../bd-conect')
+const sql = require('../bd-conect');
 
 exports.get = async (req, res, next) => {
-    let allUsers = sql.creationDatabase('SELECT * FROM User')
-    try{
-        res.status(200).send(allUsers);
-    } catch (e) {
-        res.status(500).send({
-            message: 'Error on request'
-        })
-    }
-}
+  let allUsers = sql.creationDatabase('SELECT * FROM User');
+  try {
+    res.status(200).send(allUsers);
+  } catch (e) {
+    res.status(500).send({
+      message: 'Error on request',
+    });
+  }
+};
 
 exports.post = async (req, res, next) => {
-    let name = req.body.name;
-    let email = req.body.email;
-    let password = req.body.password;
-    sql.creationDatabase(`INSENT IN TO User(name, email, password) VALUES (${name}, ${email}, ${password})`, res)
-
-}
+  let user = new userModel(req.body)
+  sql.creationDatabase(
+    `INSENT IN TO User(name, email, password) VALUES (${user.name}, ${user.email}, ${user.password})`,
+    res
+  );
+};
